181fa50716c09c36d414028c0cd4445b8ea8a8fa
joseph0926 <joseph0926@naver.com>
2025-03-15 08:43:52 +0900

    [refactor] prettier & lint

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,20 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Geist, Geist_Mono } from 'next/font/google';
+import './globals.css';
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
 });
 
 export const metadata: Metadata = {
-  title: "김영훈 블로그",
-  description: "프론트엔드 개발자 김영훈의 블로그입니다.",
+  title: '김영훈 블로그',
+  description: '프론트엔드 개발자 김영훈의 블로그입니다.',
 };
 
 export default function RootLayout({
