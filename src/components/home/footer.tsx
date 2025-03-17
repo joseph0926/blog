58571b4b2948fb605ef510ed7f684471003e2171
joseph0926 <joseph0926@naver.com>
2025-03-18 07:46:44 +0900

    [feat] home - footer 추가

--- src/components/home/footer.tsx
+++ src/components/home/footer.tsx
@@ -1,9 +1,26 @@
+import { INFO } from '@/constants/info';
 import { Container } from '../ui/container';
+import { LogoIcon } from '../ui/icons';
 
 export const Footer = () => {
   return (
-    <Container size="md" className="bg-blue-500">
-      tasfafs
+    <Container as="footer" size="md" className="mt-12 mb-6">
+      <LogoIcon />
+      <div className="flex items-center gap-2 text-sm font-medium">
+        <span className='after:px-2 after:content-["·"]'>
+          &copy;2025 joseph0926
+        </span>
+        <span className='after:px-2 after:content-["·"]'>
+          <a href={INFO.GITHUB} target="_blank">
+            GitHub
+          </a>
+        </span>
+        <span>
+          <a href={INFO.LINKEDIN} target="_blank">
+            Linkedin
+          </a>
+        </span>
+      </div>
     </Container>
   );
 };
---
+++ src/constants/info.ts
@@ -0,0 +1,4 @@
+export const INFO = {
+  GITHUB: 'https://github.com/joseph0926',
+  LINKEDIN: 'https://www.linkedin.com/in/joseph0926',
+};
