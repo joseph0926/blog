dcfa7d8d398202a29cd13d8cb7f1c90cd4652171
joseph0926 <joseph0926@naver.com>
2025-03-19 07:32:46 +0900

    [chore] common - mdx 설정

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,15 @@
 import type { NextConfig } from 'next';
+import createMDX from '@next/mdx';
 
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [{ hostname: 's3-alpha-sig.figma.com', protocol: 'https' }],
   },
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
 };
 
-export default nextConfig;
+const withMDX = createMDX({
+  // Add markdown plugins here, as desired
+});
+
+export default withMDX(nextConfig);
