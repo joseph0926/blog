d8bc7edc607fcb9ad5f0ae3086ff20d6c7fc53c2
joseph0926 <joseph0926@naver.com>
2025-03-22 10:14:45 +0900

    [refactor] common - mdx 수정

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -2,7 +2,7 @@ import { PostHeaderLoading } from '@/components/loading/post-header.loading';
 import { PostHeader } from '@/components/post/post-header';
 import { Container } from '@/components/ui/container';
 import { getPostContent } from '@/services/post.service';
-import { MDXRemote } from 'next-mdx-remote/rsc';
+import { compileMDX } from 'next-mdx-remote/rsc';
 import { Suspense } from 'react';
 
 export default async function PostPage({
@@ -13,12 +13,19 @@ export default async function PostPage({
   const { slug } = await params;
   const { source } = await getPostContent(slug);
 
+  const { content } = await compileMDX({
+    source,
+    options: { parseFrontmatter: true },
+  });
+
   return (
     <Container as="main" size="lg" className="relative">
       <Suspense fallback={<PostHeaderLoading />}>
         <PostHeader slug={slug} />
       </Suspense>
-      <MDXRemote source={source} />
+      <article className="prose dark:prose-invert max-w-none py-8">
+        {content}
+      </article>
     </Container>
   );
 }
--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -2,6 +2,17 @@ import type { MDXComponents } from 'mdx/types';
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
+    h1: ({ children }) => (
+      <h1 className="my-4 text-4xl font-bold">{children}</h1>
+    ),
+    h2: ({ children }) => (
+      <h2 className="my-3 text-3xl font-semibold">{children}</h2>
+    ),
+    h3: ({ children }) => (
+      <h3 className="my-2 text-2xl font-medium">{children}</h3>
+    ),
+    ul: ({ children }) => <ul className="list-disc pl-4">{children}</ul>,
+    ol: ({ children }) => <ol className="list-decimal pl-4">{children}</ol>,
     ...components,
   };
 }
