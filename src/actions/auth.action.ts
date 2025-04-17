8fc293acf2580126fd2de7de9881f5a3f25a6e04
김영훈 <joseph0926@naver.com>
2025-04-17 18:37:55 +0900

    feat.signin: 로그인 로직 구현

--- src/actions/auth.action.ts
+++ src/actions/auth.action.ts
@@ -1,10 +1,14 @@
 'use server';
 
 import { Prisma } from '@prisma/client';
-import { generateHash } from '@/lib/auth/password';
+import { generateHash, verifyPassword } from '@/lib/auth/password';
 import { prisma } from '@/lib/prisma';
 import { authSchema, type AuthSchemaType } from '@/schemas/auth.schema';
 import { ActionResponse } from '@/types/action.type';
+import { UserResponse } from '@/types/user.type';
+import { generateRefreshToken, signAccessToken } from '@/lib/auth/token';
+import { cookies } from 'next/headers';
+import { delay } from '@/lib/utils';
 
 export const signup = async (
   payload: AuthSchemaType,
@@ -56,4 +60,95 @@ export const signup = async (
       status: 500,
     };
   }
+};
+
+export const signin = async (
+  payload: AuthSchemaType,
+): Promise<ActionResponse<{ user: UserResponse }>> => {
+  const parsed = authSchema.safeParse(payload);
+  if (!parsed.success) {
+    return {
+      success: false,
+      message: parsed.error.errors[0]?.message ?? '유효하지 않은 입력입니다.',
+      data: null,
+      status: 400,
+    };
+  }
+
+  const { email, password } = parsed.data;
+
+  let user: { id: string; email: string; password: string } | null = null;
+  try {
+    user = await prisma.user.findUnique({
+      where: { email },
+      select: { id: true, email: true, password: true },
+    });
+  } catch (err) {
+    console.error(err);
+    // logger.error({ err, scope: 'signin#findUnique' });
+    return {
+      success: false,
+      message: '서버 오류로 로그인에 실패했습니다.',
+      data: null,
+      status: 500,
+    };
+  }
+
+  if (!user) {
+    await delay(1200);
+    return {
+      success: false,
+      message: '이메일 또는 비밀번호가 올바르지 않습니다.',
+      data: null,
+      status: 401,
+    };
+  }
+
+  const isMatch = await verifyPassword(password, user.password);
+  user.password = '';
+
+  if (!isMatch) {
+    await delay(1200);
+    return {
+      success: false,
+      message: '이메일 또는 비밀번호가 올바르지 않습니다.',
+      data: null,
+      status: 401,
+    };
+  }
+
+  const accessToken = signAccessToken(user.id);
+  const { token, expires } = generateRefreshToken();
+
+  const ck = await cookies();
+  ck.set({
+    name: 'accessToken',
+    value: accessToken,
+    httpOnly: true,
+    secure: true,
+    sameSite: 'lax',
+    maxAge: 600,
+    path: '/',
+  });
+  ck.set({
+    name: 'refreshToken',
+    value: token,
+    httpOnly: true,
+    secure: true,
+    sameSite: 'strict',
+    maxAge: expires,
+    path: '/auth',
+  });
+
+  return {
+    data: {
+      user: {
+        id: user.id,
+        email: user.email,
+      },
+    },
+    success: true,
+    status: 200,
+    message: '로그인에 성공하였습니다.',
+  };
 };
--- src/lib/auth/password.ts
+++ src/lib/auth/password.ts
@@ -1,5 +1,9 @@
-import { hash as argonHash } from '@node-rs/argon2';
+import { hash as argonHash, verify as argonVerify } from '@node-rs/argon2';
 
 export const generateHash = async (plain: string) => {
   return await argonHash(plain, { memoryCost: 48 << 10, timeCost: 3 });
+};
+
+export const verifyPassword = async (plain: string, hashed: string) => {
+  return await argonVerify(hashed, plain);
 };
