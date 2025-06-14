9f6bd37912537de54a68a5b771778ae4d4e8a923
joseph0926 <joseph0926@naver.com>
2025-06-14 09:37:43 +0900

    chore/ci/pref: lighthouse ci 추가

---
+++ .lighthouserc.js
@@ -0,0 +1,16 @@
+// @ts-check
+
+/** @type {import('@lhci/cli').LighthouseCiRc} */
+module.exports = {
+  ci: {
+    collect: {
+      url: ['http://localhost:3000/'],
+      numberOfRuns: 3,
+      settings: { preset: 'desktop' },
+    },
+    upload: { target: 'filesystem', outputDir: '.lhci' },
+    assert: {
+      assertions: { performance: ['error', { minScore: 0.9 }] },
+    },
+  },
+};
