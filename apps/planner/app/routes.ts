391a35119e7cef38a601149572b9c2c6ad19265a
joseph0926 <joseph0926@naver.com>
2025-09-13 13:33:29 +0900

    apps.planner: 기본 routes 정의

--- apps/planner/app/routes.ts
+++ apps/planner/app/routes.ts
@@ -1,3 +1,17 @@
-import { index, type RouteConfig } from '@react-router/dev/routes';
+import {
+  index,
+  // route,
+  layout,
+  type RouteConfig,
+} from '@react-router/dev/routes';
 
-export default [index('routes/home.tsx')] satisfies RouteConfig;
+export default [
+  layout('routes/root.tsx', [
+    index('routes/home.tsx'),
+    // route('queue', 'routes/queue.tsx'),
+    // route('review', 'routes/review.tsx'),
+
+    // route('api/capture', 'routes/api/capture.tsx'),
+    // route('api/captures', 'routes/api/captures.tsx'),
+  ]),
+] satisfies RouteConfig;
